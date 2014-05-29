c4479ae2f380d94d2f70762281ff8c1a2b5861f7
Vijay Tyagi <vijaytyagi.vt@gmail.com>
2014-05-29 17:12:30 +0530

    Update poll.js

--- app/scripts/controllers/poll.js
+++ app/scripts/controllers/poll.js
@@ -1,10 +1,74 @@
 'use strict';
 
 angular.module('iPollApp')
-  .controller('PollCtrl', function ($scope, $stateParams, $ionicSideMenuDelegate ) {
+  .controller('PollCtrl', 
+  	function ($scope, $state, $timeout, $stateParams,$ionicModal, $ionicLoading, $ionicSideMenuDelegate, $ionicNavBarDelegate, Pollservice ) {
     console.log('poll state activated');
-    $scope.toggleCategories = function(){
+    
+  	
+    $scope.activePollCategory  =  Pollservice.getPollCategoryById($state.params.category);
+    $scope.polls = Pollservice.getPollsByCategory($scope.activePollCategory.id);
+
+    $scope.toggleCategories = function() {
     	$ionicSideMenuDelegate.toggleLeft();
+    	
     };
+
+    $scope.selectPolls = function( categoryId ) {
+    	//$scope.showLoader();
+    	$scope.activePollCategory = Pollservice.getPollCategoryById(categoryId);
+    	$scope.polls = Pollservice.getPollsByCategory(categoryId);
+    	
+    	
+    	//$timeout(function(){
+    		$ionicSideMenuDelegate.toggleLeft(false);
+    		$state.go("poll.categories.category", {category: categoryId});
+    		//$ionicLoading.hide();
+    	//}, 500);
+    	
+    };
+
+    $scope.categories = Pollservice.getPollCategories();
+
+    $ionicModal.fromTemplateUrl('views/new_task.html', function(modal) {
+	    $scope.taskModal = modal;
+	  }, {
+	    scope: $scope,
+	    animation: 'slide-in-up'
+	  });
+    
+     $scope.newTask = function() {
+     	
+	    $scope.taskModal.show();
+	 };
+
+	  $scope.closeNewTask = function() {
+	    $scope.taskModal.hide();
+	  };
+
+	  $scope.showLoader = function(){
+	  	$ionicLoading.show({
+	  		content: '<div class = "cust-loader"><img src = "images/cloud-icon.png" class = "animation-rotate" /></div>',
+    		animation: 'fade-in',
+    		showBackdrop: false,
+    		showDelay: 0
+	  	});
+	  };
+
+	  $scope.hideLoader = function(){
+	  	$ionicLoading.hide();
+	  };
+	  $scope.showPoll = function( pollId ){
+	  	console.log('show poll called');
+	  	$scope.showLoader();
+	  	
+	  	$timeout(function(){
+
+	  		$state.go("poll.show", {pollID: pollId});
+	  		$scope.hideLoader();
+
+	  	}, 1000);
+	  
+	  };
 
   });
